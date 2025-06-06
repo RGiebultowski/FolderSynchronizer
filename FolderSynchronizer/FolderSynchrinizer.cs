3cfbbca4b7df5709f6b87c8ba4f25f4fe5b83f75
dxt <raf.giebultowski@gmail.com>
2025-06-06 14:34:22 +0200

    wip

--- FolderSynchronizer/FolderSynchrinizer.cs
+++ FolderSynchronizer/FolderSynchrinizer.cs
@@ -10,11 +10,28 @@ namespace FolderSynchronizer
     {
         private readonly string sourcePath;
         private readonly string replicaPath;
+        private readonly Logger logger;
 
-        public FolderSynchrinizer(string sourcePath, string replicaPath)
+        public FolderSynchrinizer(string sourcePath, string replicaPath, Logger logger)
         {
             this.sourcePath = sourcePath;
             this.replicaPath = replicaPath;
+            this.logger = logger;
+        }
+
+        public void Synchronize() 
+        {
+            CreateDirectory(sourcePath);
+            CreateDirectory(replicaPath);
+        }
+
+        public void CreateDirectory(string path)
+        {
+            var message = Directory.Exists(path) ?
+                $"Source Folder Exists: {path}" :
+                $"Created folder: {Directory.CreateDirectory(path).FullName}";
+
+            logger.Log(message);
         }
     }
 }
--- FolderSynchronizer/Program.cs
+++ FolderSynchronizer/Program.cs
@@ -4,7 +4,13 @@
     {
         if (args.Length != 4)
         {
-            Console.WriteLine("Usage: FolderSynchronizer.exe <sourcePath> <replicaPath> <interval> <logFile>");
+            Console.WriteLine("Usage: FolderSynchronizer.exe <sourcePath> <replicaPath> <intervalForSync> <logFile>");
+            return;
         }
+
+        string sourcePath = args[0];
+        string replicaPath = args[1];
+        int intervalSecs = int.Parse(args[2]);
+        string logFile = args[3];
     }
 }
